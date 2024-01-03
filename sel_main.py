884925aa27fd50e82d44e2fc7941c195af45b99c
Lambert <lanniao77@hotmail.com>
2024-01-03 11:12:35 +0800

    更新可使用

--- sel_main.py
+++ sel_main.py
@@ -1,22 +1,26 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt,QDate
-from PyQt5.QtWidgets import QMainWindow,QMessageBox
+from PyQt5.QtCore import Qt,QCoreApplication
+from PyQt5.QtWidgets import QMainWindow
 from Ui_init import Ui_MainWindow 
 import call_main,main_qm
-from PyQt5.QtGui import QStandardItemModel,QStandardItem
 from qfluentwidgets import PushButton
 
 class CallMain(QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
-        #self.Call_GMY = call_main.CallMain()
-        #self.PushGMY= self.findChild(PushButton,'pushGMY')
-        #self.PushQM= self.findChild(PushButton,'pushQM')
-        self.PushQuit = self.findChild(PushButton,'pushQuit')
+        #获取屏幕分辨率数据
+        #screen_width = QApplication.desktop().screenGeometry().width()  
+        #screen_height = QApplication.desktop().screenGeometry().height()
+        #xfl_x =QApplication.desktop().logicalDpiX()
+        #sfl= xfl_x/96
+        #self.setGeometry(int((screen_width/3)*sfl),int((screen_height/3)*sfl),int((screen_width/4)*sfl),int((screen_height/4)*sfl))
+        #self.layoutMain =self.findChild(QtWidgets.QLayout,'layoutMain')
+        self.PushQuit = self.findChild(PushButton,'PushQuit')
         self.PushGMY.clicked.connect(self.Call_GMY)
         self.PushQM.clicked.connect(self.Call_QM)
+        self.PushQuit.clicked.connect(self.Call_Quit)
         
 
     def Call_GMY(self):
@@ -25,7 +29,11 @@ class CallMain(QMainWindow, Ui_MainWindow):
     def Call_QM(self):
         qmmain_qm.show()
 
+    def Call_Quit(self):
+        self.close()
+
 if __name__ == '__main__':
+    QCoreApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication(sys.argv)
     main=CallMain()
     main_gmy = call_main.CallMain()
